e55e1bf0c2215e3ed918c4a15d59b17d269cc714
Wakana <wakanichi13@gmail.com>
2023-09-18 12:31:36 +0400

    feat: add components

---
+++ components/events/EventList.js
@@ -0,0 +1,20 @@
+import EventItem from "./EventItem";
+
+function EventList(props) {
+  const { items } = props;
+  return (
+    <ul>
+      {items.map((event) => (
+        <EventItem
+          key={event.id}
+          id={event.id}
+          title={event.title}
+          location={event.location}
+          image={event.image}
+          date={event.date}
+        />
+      ))}
+    </ul>
+  );
+}
+export default EventList;
