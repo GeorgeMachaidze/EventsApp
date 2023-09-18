74d0b2f4628182be580d3cafbd8541ac0724b99a
Wakana <wakanichi13@gmail.com>
2023-09-18 14:27:08 +0400

    feat: add buttons

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Button from "../ui/Button";
 import classes from "./EventItem.module.css";
 
 function EventItem(props) {
@@ -27,7 +28,7 @@ function EventItem(props) {
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={exploreLink}>Explore Event</Link>
+          <Button link={exploreLink}>Explore Event</Button>
         </div>
       </div>
     </li>
