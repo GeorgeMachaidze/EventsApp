a9e2e680065fb193797327c125d982a4848ceb95
Wakana <wakanichi13@gmail.com>
2023-09-18 16:41:28 +0400

    style: single event page done

--- pages/events/[eventsId].js
+++ pages/events/[eventsId].js
@@ -1,8 +1,31 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import { Fragment } from "react";
+import EventSummary from "../../components/eventDetail/event-summary";
+import EventLogistics from "../../components/eventDetail/event-logistics";
+import EventContent from "../../components/eventDetail/event-content";
+
 function EventsDetailPage() {
+  const router = useRouter();
+
+  const eventsId = router.query.eventsId;
+  const event = getEventById(eventsId);
+  console.log(router.query);
+  if (!event) {
+    return <p>No Event Found</p>;
+  }
+
   return (
-    <div>
-      <h1>Events Detail Page</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>{<p>{event.description}</p>}</EventContent>
+    </Fragment>
   );
 }
 export default EventsDetailPage;
